a3cb6eecd07ef450bb3941e1a3a09a6427d25fd3
Carlos Palol <carlos.palol@awarepixel.com>
2013-11-16 05:56:37 +0100

    Distutils config

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
-from setuptools import setup
+from distutils.core import setup
+
+
+DESCRIPTION = """
+Python money class with optional CLDR-backed locale-aware formatting and an extensible currency exchange solution.
+"""
 
 setup(
-    name="python-money2",
-    description="",
-    version="0.1",
-    author="Carlos Palol",
-    author_email="carlos.palol@awarepixel.com",
-    url="",
+    name='money',
+    description='Python Money Class',
+    long_description=DESCRIPTION,
+    version='1.0b1',
+    author='Carlos Palol',
+    author_email='carlos.palol@awarepixel.com',
+    url='http://www.awarepixel.com',
+    license='MIT',
     packages=[
-        'money'
+        'money',
+        'money.tests',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries',
     ]
 )
