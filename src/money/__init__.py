2a88da2cf63bc7eb658f336e5be48262b3e30e8b
Carlos Palol <carlos.palol@awarepixel.com>
2014-03-31 07:08:42 +0200

    Move source into src and src-py2

---
+++ src/money/__init__.py
@@ -0,0 +1,9 @@
+"""
+Python money class with optional CLDR-backed locale-aware formatting
+and an extensible currency exchange solution.
+"""
+from .money import Money, XMoney
+from .exchange import xrates
+
+
+__version__ = '1.1.0-dev'
---
+++ src/money/tests/__init__.py
@@ -0,0 +1,7 @@
+"""
+Money unittests
+
+Run with:
+$ python -m unittest
+
+"""
