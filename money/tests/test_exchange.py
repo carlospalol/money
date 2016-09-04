1ca76aa1de7ac54b488bc2b4c8b2e193d9329363
Carlos Palol <carlos.palol@awarepixel.com>
2016-09-05 01:30:24 +0200

    Various improvements to tests

--- money/tests/test_exchange.py
+++ money/tests/test_exchange.py
@@ -2,7 +2,9 @@
 """
 Money exchange unittests
 """
-import abc
+# RADAR: Python2
+from __future__ import absolute_import
+
 from decimal import Decimal
 import unittest
 
@@ -76,55 +78,22 @@ class TestExchangeRatesSetup(unittest.TestCase):
         self.assertEqual(another.rate('AAA'), Decimal('100'))
 
 
-# RADAR: Python2
-@money.six.add_metaclass(abc.ABCMeta)
-class BackendTestBase(object):
-    @abc.abstractmethod
-    def test_base_property(self):
-        pass
-
-    @abc.abstractmethod
-    def test_rate(self):
-        pass
-
-    @abc.abstractmethod
-    def test_quotation(self):
-        pass
-
-    @abc.abstractmethod
-    def test_unavailable_rate_returns_none(self):
-        pass
-
-    @abc.abstractmethod
-    def test_unavailable_quotation_returns_none(self):
-        pass
-
-    @abc.abstractmethod
-    def test_money_conversion(self):
-        pass
-
-
-class TestSimpleBackend(BackendTestBase, unittest.TestCase):
+class TestSimpleBackend(unittest.TestCase):
     def setUp(self):
         xrates.install('money.exchange.SimpleBackend')
-
-    def load_testing_data(self):
         xrates.base = 'XXX'
         xrates.setrate('AAA', Decimal('2'))
         xrates.setrate('BBB', Decimal('8'))
 
     def test_base_property(self):
-        self.load_testing_data()
         self.assertEqual(xrates.base, 'XXX')
 
     def test_rate(self):
-        self.load_testing_data()
         self.assertEqual(xrates.rate('XXX'), Decimal('1'))
         self.assertEqual(xrates.rate('AAA'), Decimal('2'))
         self.assertEqual(xrates.rate('BBB'), Decimal('8'))
 
     def test_quotation(self):
-        self.load_testing_data()
         self.assertEqual(xrates.quotation('XXX', 'XXX'), Decimal('1'))
         self.assertEqual(xrates.quotation('XXX', 'AAA'), Decimal('2'))
         self.assertEqual(xrates.quotation('XXX', 'BBB'), Decimal('8'))
@@ -136,19 +105,18 @@ class TestSimpleBackend(BackendTestBase, unittest.TestCase):
         self.assertEqual(xrates.quotation('BBB', 'BBB'), Decimal('1'))
 
     def test_unavailable_rate_returns_none(self):
-        self.load_testing_data()
         self.assertIsNone(xrates.rate('ZZZ'))
 
     def test_unavailable_quotation_returns_none(self):
-        self.load_testing_data()
         self.assertIsNone(xrates.quotation('YYY', 'ZZZ'))
 
     def test_money_conversion(self):
-        self.load_testing_data()
         self.assertEqual(Money('10', 'AAA').to('BBB'), Money('40', 'BBB'))
         self.assertEqual(Money('10', 'BBB').to('AAA'), Money('2.5', 'AAA'))
 
     def test_base_not_set_warning(self):
+        xrates.uninstall()
+        xrates.install('money.exchange.SimpleBackend')
         with self.assertRaises(Warning):
             xrates.setrate('AAA', Decimal('2'))
 
